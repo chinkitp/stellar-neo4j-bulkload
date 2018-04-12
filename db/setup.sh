f9dbabd3bb96ebbb6fbe9229e9ad5ef8579d2850
Chinkit Patel <chinkit.patel@gmail.com>
2018-04-12 16:15:53 +0930

    changed to use bulk import

--- db/setup.sh
+++ db/setup.sh
@@ -4,9 +4,13 @@ set -e
 
 echo 'Installing neo4j apoc (plugins)..'
 
-cd plugins
+cd ./stellar-search-neo4j
+curl 'https://raw.githubusercontent.com/chinkitp/stellar-neo4j-bulkload/master/Resources/nodes/all-answers.json' | jq -r '. | [.id, .props.description, .props.created, .props.vote] | @csv ' > all-answers.csv
+neo4j-admin import --nodes:answers:Lr41MAakQRqPwVuj3j4jCg="answers-header.csv,all-answers.csv"
 
-wget https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/3.3.0.2/apoc-3.3.0.2-all.jar
+#curl 'https://raw.githubusercontent.com/chinkitp/stellar-neo4j-bulkload/master/Resources/nodes/all-answers.json' | jq -r '. | [.id, .props.description, .props.created, .props.vote, "answers", .graphs[0]] | @csv ' > all-answers.csv
+#wget https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/3.3.0.2/apoc-3.3.0.2-all.jar
+
 
 
 
